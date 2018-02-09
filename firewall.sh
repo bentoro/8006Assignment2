f7ee11f27912beb4c2a05e40b1a0abd08e6d20c6
Aing <aingaran2003@gmail.com>
2018-02-08 23:49:17 -0800

    functionality working

--- firewall.sh
+++ firewall.sh
@@ -1,25 +1,27 @@
-IP = /sbin/iptables
+IP="/sbin/iptables"
 
-FWNAME
-FWLOCATION
-INTERNALNET
-FWNIP="192.168.0.22" #IP address of firewall on the network
+#FWNAME
+#FWLOCATION
+#INTERNALNET
+FWNIP="192.168.0.11" #IP address of firewall on the network
 FWIP="192.168.10.1" #IP address of the firewall within the new network
 WORKSTATION="192.168.10.2"  #IP address of workstation using the firewall
-INTERFACE="enp3s2" 
+INTERFACE="enp3s2"
+EXTERNAL_INTERFACE="eno1"
 
-TCP_DROP=($FWIP 23 32768:32775 137:139 111 515)
+TCP_DROP=(23 32768:32775 137:139 111 515)
 UDP_DROP=(32768:32775 137:139 111 515)
-ICMP_DROP=(32768:32775 137:139 111 515)
+ICMP_DROP=() #=(32768:32775 137:139 111 515)
 
 BLOCK_IP=(192.168.0.0/24)
-TCP_ALLOW
-UDP_ALLOW
-ICMP_ALLOW
+ALLOW=(80 22 53 67 68)
+TCP_ALLOW=(22 80 443)
+UDP_ALLOW=(22 53 67 68)
+#ICMP_ALLOW
+
 
 Cleanup(){
 	$IP -F
-
 	$IP -X
 }
 
@@ -30,102 +32,118 @@ SetDefaults(){
 }
 
 SetFilters(){
-    #drop all incoming/outgoing packets for the firewall
-    $IP -A INPUT ALL DROP 
-    $IP -A OUTPUT ALL DROP 
-
 	#accept fragments
 	$IP -A FORWARD -f -j ACCEPT
 	#allow stateful add to all for loops
+    #dont understand properly
+    #$IP -A FORWARD -p all -d $BLOCK_IP -j DROP
 
     #prerouting
-    for i in "${ALLOW[@]}"
+    for i in "${TCP_ALLOW[@]}"
     do 
         :
-        $IP -t nat -A PREROUTING -i $INTERFACE --destination-port $i -j DNAT --to-destination $WORKSTATION:$i
+        $IP -t nat -A PREROUTING -p tcp -i $EXTERNAL_INTERFACE --destination-port $i -j DNAT --to-destination $WORKSTATION:$i
+
     done
+
+    #prerouting
+#    for i in "${UDP_ALLOW[@]}"
+#    do
+#        :
+#        $IP -t nat -A PREROUTING -p udp -i $EXTERNAL_INTERFACE --destination-port $i -j DNAT --to-destination $WORKSTATION:$i
+#    done
+
+
+    #iptables -t nat -A POSTROUTING -i $INTERFACE -o $EXTERNAL_INTERFACE -m state --state NEW,ESTABLISHED -j SNAT --to-sourc $FWNIP
+    #iptables -t nat -A PREROUTING -i $EXTERNAL_INTERFACE -o $INTERFACE -m state --state NEW,ESTABLISHED -j DNAT --to-destination $WORKSTATION
 
     #postrounting
-    for i in "${ALLOW[@]}"
-    do
-        :
-        $IP -t nat -A POSTROUTING -o $INTERFACE -j SNAT --to-source $FWNIP
-    done
-
+    $IP -t nat -A POSTROUTING -o $EXTERNAL_INTERFACE -j SNAT --to-source $FWNIP
 
 	#set minimum delay and ftp data to maximum throughput
 	$IP -A PREROUTING -t mangle -p tcp --sport ssh -j TOS --set-tos Minimize-Delay
-	$ip -A PREROUTING -t mangle -p tcp --sport ftp -j TOS --set-tos Minimize-Delay
-	$ip -A PREROUTING -t mangle -p tcp --sport ftp-data -j TOS --set-tos Maximize-Throughput
+	$IP -A PREROUTING -t mangle -p tcp --sport ftp -j TOS --set-tos Minimize-Delay
+	$IP -A PREROUTING -t mangle -p tcp --sport ftp-data -j TOS --set-tos Maximize-Throughput
 	$IP -A OUTPUT -t mangle -p tcp --sport ssh -j TOS --set-tos Minimize-Delay
-	$ip -A OUTPUT -t mangle -p tcp --sport ftp -j TOS --set-tos Minimize-Delay
-	$ip -A OUTPUT -t mangle -p tcp --sport ftp-data -j TOS --set-tos Maximize-Throughput
+	$IP -A OUTPUT -t mangle -p tcp --sport ftp -j TOS --set-tos Minimize-Delay
+	$IP -A OUTPUT -t mangle -p tcp --sport ftp-data -j TOS --set-tos Maximize-Throughput
 
     #block all incoming syns
     $IP -A INPUT -p tcp ! --syn -m state --state NEW -j DROP
     #block all incoming syn fin packets
     $IP -A INPUT -p tcp --tcp-flags ALL SYN,FIN -j DROP
 
+    #
+
     for i in "${TCP_DROP[@]}"
     do
         :
         $IP -A FORWARD -p tcp -m tcp --sport $i -j DROP
+        $IP -A FORWARD -p tcp -m tcp --dport $i -j DROP
     done
 
    for i in "${UDP_DROP[@]}"
     do
         :
-        $IP -A FORWARD -p tcp -m tcp --sport $i -j DROP
+        $IP -A FORWARD -p udp -m udp --sport $i -j DROP
+        $IP -A FORWARD -p udp -m udp --dport $i -j DROP
     done
 
    for i in "${ICMP_DROP[@]}"
     do
         :
-        $IP -A FORWARD -p tcp -m tcp --sport $i -j DROP
+        $IP -A FORWARD -p icmp -m icmp --sport $i -j DROP
+        $IP -A FORWARD -p icmp -m icmp --dport $i -j DROP
     done
 
    for i in "${TCP_ALLOW[@]}"
     do
         :
         $IP -A FORWARD -p tcp -m tcp --sport $i -j ACCEPT
-    done
-
-   for i in "${TCP_ALLOW[@]}"
-    do
-        :
-        $IP -A FORWARD -p tcp -m tcp --sport $i -j ACCEPT
+        $IP -A FORWARD -p tcp -m tcp --dport $i -j ACCEPT
+        #create a state for established connections
+        $IP -A FORWARD -o $INTERFACE -p tcp --sport $i -m conntrack --ctstate \
+            NEW,ESTABLISHED -j ACCEPT
+        $IP -A FORWARD -o $INTERFACE -p tcp --dport $i -m conntrack --ctstate \
+            NEW,ESTABLISHED -j ACCEPT
     done
 
    for i in "${UDP_ALLOW[@]}"
     do
         :
-        $IP -A FORWARD -p tcp -m tcp --sport $i -j ACCEPT
+        $IP -A FORWARD -p udp -m udp --sport $i -j ACCEPT
+        $IP -A FORWARD -p udp -m udp --dport $i -j ACCEPT
+        $IP -A FORWARD -o $INTERFACE -p tcp --sport $i -m conntrack --ctstate \
+            NEW,ESTABLISHED -j ACCEPT
+        $IP -A FORWARD -o $INTERFACE -p udp --dport $i -m conntrack --ctstate \
+            NEW,ESTABLISHED -j ACCEPT
     done
 
    for i in "${ICMP_ALLOW[@]}"
     do
         :
-        $IP -A FORWARD -p tcp -m tcp --sport $i -j ACCEPT
+        $IP -A FORWARD -p icmp -m icmp --sport $i -j ACCEPT
+        $IP -A FORWARD -p icmp -m icmp --dport $i -j ACCEPT
     done
 }
 Firewall(){
 
         #Setup standalone firewall with ifconfig
-        localIP="192.168.0.15"
-
-        ifconfig enp3s2 192.168.10.1 up
+        ifconfig $INTERFACE 192.168.10.1 up
         echo "1" >/proc/sys/net/ipv4/ip_forward
-        route add -net 192.168.0.0 netmask 255.255.255.0 gw $localIP
+        route add -net 192.168.0.0 netmask 255.255.255.0 gw $FWNIP
         route add -net 192.168.10.0/24 gw 192.168.10.1
-        Cleanup
-        SetDefaults
-        SetFilters
 }
 Workstation(){
         #firewall
-        ifconfig eno1 down
-        ifconfig enp3s2 192.168.10.2 up
+        ifconfig $EXTERNAL_INTERFACE down
+        ifconfig $INTERFACE 192.168.10.2 up
         route add default gw 192.168.10.1
+}
+Setup(){
+        Cleanup
+        SetDefaults
+        SetFilters
 }
     if [ "$#" -ne 1 ]; then
         echo "Options workstation/firewall"
@@ -133,5 +151,6 @@ Workstation(){
     fi
     case $1 in
         "workstation") workstation ;;
-        "firewall") firewall
+        "firewall") Firewall ;;
+        "setup") Setup ;;
     esac
--- test.sh
+++ test.sh
@@ -1,8 +1,82 @@
-$sport=(80 22)
+tcp=(32768 32771 32775 137 138 139 111 515 23)
 
-for i in "${sport[@]}"
+for i in "${tcp[@]}"
 do
 	:
 	hping3 $1 -S -p $i -c 1 >> output
 	echo $?
+	if [  $? -eq 0 ]; then
+	    echo "$i passed the test"
+	else
+	    echo "$i failed the test"
+	fi
 done
+
+icmp=(8)
+
+for i in "${icmp[@]}"
+do
+	:
+	hping3 $1 -S -p $i -1 -c 1 >> output
+	echo $?
+	if [  $? -eq 0 ]; then
+	    echo "$i passed the icmp test"
+	else
+	    echo "$i failed the icmp test"
+	fi
+done
+
+udp=(67)
+
+for i in "${udp[@]}"
+do
+	:
+	hping3 $1 -S -p $i -2 -c 1 >> output
+	echo $?
+	if [  $? -eq 0 ]; then
+	    echo "$i passed the udp test"
+	else
+	    echo "$i failed the udp test"
+	fi
+done
+
+dport=(80)
+
+for i in "${dport[@]}"
+do
+	:
+	hping3 $1 -SF -p $i -c 1 >> output
+	echo $?
+	if [  $? -eq 0 ]; then
+	    echo "$i passed the test for SYN FIN"
+	else
+	    echo "$i failed the test for SYN FIN"
+	fi
+done
+
+
+	hping3 $1 -S -p 50000 -c 1 >> output
+	echo $?
+	if [  $? -eq 0 ]; then
+	    echo "$i passed the test for SYN FIN"
+	else
+	    echo "$i failed the test for SYN FIN"
+	fi
+
+#fragment flags - set more fragments
+	hping3 $1 -S -p 22 -c 1 -x >> output
+	echo $?
+	if [  $? -eq 0 ]; then
+	    echo "passed the test for fragment testing"
+	else
+	    echo "failed the test for fragment testing"
+	fi
+
+#Don't fragment flags
+    hping3 $1 -S -p 22 -c 1 -y >> output
+	echo $?
+	if [  $? -eq 0 ]; then
+	    echo "passed the test for DON'T fragment testing"
+	else
+	    echo "failed the test for DON'T fragment testing"
+	fi
